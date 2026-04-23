61aeb1ccccf8361a58be7ddd74e3973cda9e16ae
Martijang <seungwoogo1123@gmail.com>
2026-04-23 13:49:07 +0800

    worker threads edited

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,8 @@ mod checker;
 
 use checker::UrlChecker;
 
-#[tokio::main]
+//worker threads are two for now. I might fix this later
+#[tokio::main(worker_threads = 2)]
 async fn main(){
     UrlChecker::new().run().await;
 }
