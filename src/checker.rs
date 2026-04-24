f76def01a7286216b2d6c244b4e2cf52103566d8
Martijang <seungwoogo1123@gmail.com>
2026-04-24 19:11:44 +0800

    body argument added

--- src/checker.rs
+++ src/checker.rs
@@ -2,19 +2,26 @@ use clap::Parser;
 use reqwest::StatusCode;
 
 ///Cli url checker
-#[derive(Parser, Clone)]
+#[derive(Parser)]
+#[command(about, long_about = None)]
 struct Cli{
     ///Url(s)
     #[clap(short, num_args = 1..)]
     urls: Vec<String>,
     ///Send request as POST method (default is GET)
-    #[clap(short, long)]
-    post: Option<bool>
+    #[arg(short, long)]
+    post: Option<bool>,
+
+    ///Body option for POST request if POST method is not used
+    ///it will be replaced with default value. (Default is None)
+    #[arg(short, long)]
+    body: Option<String>
 }
 
 pub struct UrlChecker{
     urls: Vec<String>,
     post: bool,
+    body: Option<String>
 }
 
 impl UrlChecker {
@@ -22,9 +29,9 @@ impl UrlChecker {
         let cli = Cli::parse();
         if let Some(p) = cli.post {
             let post = p;
-            Self { urls: cli.urls, post}
+            Self { urls: cli.urls, post, body: cli.body }
         }else{
-            Self { urls: cli.urls, post: false }
+            Self { urls: cli.urls, post: false, body: None }
         }
     }
 
@@ -34,9 +41,10 @@ impl UrlChecker {
         //cannot use iterator due to error: borrowed data escapes outside of method
         loop{
             let post = self.post;
+            let body = self.body.clone(); //cloned due to error: borrowed data escapes outside of method(again)
             if let Some(url) = self.urls.pop(){
                 let handler = tokio::spawn(async move {
-                    request(&url, post).await;
+                    request(&url, post, body).await;
                 });
                 t_vec.push(handler);
             }else{
@@ -52,19 +60,31 @@ impl UrlChecker {
 
         Ok(response.status())
     }
-    async fn send_post_request(url: &String) -> anyhow::Result<StatusCode>{
-        let response = reqwest::Client::new()
-        .post(url)
-        .send()
-        .await?;
-        
-        Ok(response.status())
+
+    //taking body inseated of borrow
+    //due to error: the trait bound `Body: From<&std::string::String>` is not satisfied
+    async fn send_post_request(url: &String, body: Option<String>) -> anyhow::Result<StatusCode>{
+        if let Some(body) = body{
+            let response = reqwest::Client::new()
+            .post(url)
+            .body(body)
+            .send()
+            .await?;
+            Ok(response.status())
+        }else{
+            let response = reqwest::Client::new()
+            .post(url)
+            .send()
+            .await?;
+
+            Ok(response.status())
+        }
     }
 }
 
-async fn request(url: &String, post: bool){
+async fn request(url: &String, post: bool, body: Option<String>){
     if post == true {
-        match UrlChecker::send_post_request(url).await{
+        match UrlChecker::send_post_request(url, body).await{
             Ok(code) => println!("url: {url} {code}"),
             Err(e) => eprintln!("url: {url}, request failed with {e}")
         }
