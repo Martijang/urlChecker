a047b9ba63c6a928cf019ee3ca17cdecd98c5a56
Martijang <seungwoogo1123@gmail.com>
2026-05-03 05:44:05 +0800

    main fn now returns anyhow reasult

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,12 @@
 
 mod checker;
 
+use anyhow::Ok;
 use checker::UrlChecker;
 
 //worker threads are two for now. I might fix this later
 #[tokio::main(worker_threads = 2)]
-async fn main(){
-    UrlChecker::new().run().await;
+async fn main() -> anyhow::Result<()>{
+    UrlChecker::new()?.run().await;
+    Ok(())
 }
